5bfff7816ba2fb0d4e0604a8b83a64291368666a
Elias Garcia <129087002+Elias-garcia0@users.noreply.github.com>
2023-08-07 10:27:21 -0300

    subgrupo7

---
+++ README.md
@@ -0,0 +1,5 @@
+# Despliega el JSON!
+
+Deberán modificar el código que se encuentra en el archivo *js/script.js* para que al momento de cargar la página, muestre nombre y apellido de los estudiantes cargados en *json/data.json*.
+
+Importante: Para poder realizar fetch al archivo JSON se debe ejecutar el código con la extensión LiveServer.
---
+++ js/script.js
@@ -0,0 +1,21 @@
+// En este archivo no utilizamos el evento "DOMContentLoaded", ya que se colocó el atributo "defer" en la importación del script,
+// que nos soluciona el problema de los elementos no cargados del DOM. Más info => https://www.w3schools.com/tags/att_script_defer.asp
+
+const DATA_URL = "json/data.json"; // URL que contiene los datos que queremos mostrar
+
+const container = document.getElementById("container"); // "Traemos" utilizando el DOM el div de id "container" para colocar la información en él
+
+/**
+ * Función que recibe por parámetro un array con los datos que se mostrarán en el DOM
+ * Los datos se mostrarán dentro del div de id "container" y por cada ítem se está creando un nuevo párrafo donde se
+ * imprime el campo "name" y el campo "lastname" separados por un espacio
+ */
+function showData(dataArray) {
+  // El for itera sobre los elementos del array
+  for (const item of dataArray) {
+    // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
+    container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
+  }
+}
+
+// Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
