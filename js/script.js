d0604a36c18fa7c245a517a6e83880bf1f40b173
fauslo <136310613+fauslo@users.noreply.github.com>
2023-08-07 12:22:08 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,3 +19,16 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+
+async function showJSON() {
+  let response = await fetch(DATA_URL);
+  if (response.ok) {
+    let responseContents = await response.json();
+    showData(responseContents.students);
+  }
+    else {
+      alert("HTTP error" + response.status);
+    }
+  }
+
+  showJSON(DATA_URL);
